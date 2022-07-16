42920321fb5de08e8ce489161c02d05a35d67afd
andersonmares <andersonmares@gmail.com>
2022-07-16 20:30:37 -0300

    Subindo a estrutra inicial para o repo

---
+++ src/test/java/com/anderson/bookstore/BookstoreApplicationTest.java
@@ -0,0 +1,13 @@
+package com.anderson.bookstore;
+
+import org.junit.jupiter.api.Test;
+
+public class BookstoreApplicationTest {
+
+    @Test
+    public void testMain() throws Exception {
+        BookstoreApplication.main(new String[]{"args"});
+    }
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
