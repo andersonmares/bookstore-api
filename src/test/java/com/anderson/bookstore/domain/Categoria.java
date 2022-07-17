2021062daf7435252432a2574d1738743df071d5
andersonmares <andersonmares@gmail.com>
2022-07-16 21:24:48 -0300

    Classes de dominio finalizadas

--- src/test/java/com/anderson/bookstore/domain/Categoria.java
+++ src/test/java/com/anderson/bookstore/domain/Categoria.java
@@ -1,23 +1,33 @@
 package com.anderson.bookstore.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @EqualsAndHashCode
-public class Categoria {
+@Entity
+public class Categoria implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Livro> livros = new ArrayList<>();
 
     private String nome;
 
     private String descicao;
-
-    private List<Livro> livros = new ArrayList<>();
 
 }
--- src/test/java/com/anderson/bookstore/domain/Livro.java
+++ src/test/java/com/anderson/bookstore/domain/Livro.java
@@ -1,23 +1,34 @@
 package com.anderson.bookstore.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-import java.util.Objects;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @EqualsAndHashCode
-public class Livro {
+@Entity
+public class Livro implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
 
     private String titulo;
 
     private String nome_autor;
 
     private String texto;
-
-    private Categoria categoria;
 
 }
